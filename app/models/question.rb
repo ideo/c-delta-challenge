f9879d8952145f78110e0efd9c4bc1629eb755e7
Tal Safran <tal@talsafran.com>
2017-06-21 13:26:06 -0700

    Allow questions to not have question choices

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ApplicationRecord
   has_many :choices, class_name: 'QuestionChoice', inverse_of: :question
 
   validates :title, presence: true
-  validates :choices, presence: true
 
   accepts_nested_attributes_for(:choices)
 end
